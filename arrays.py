4d5559d414d1166be7d7012db99a4c11719aa144
parjinder <parjinderpannu@gmail.com>
2024-10-15 16:13:17 -0700

    arrays.py

--- arrays.py
+++ arrays.py
@@ -1,9 +1,15 @@
-import traceback
-
+# import traceback
 new_list = [1, 2, 3, 4, 5]
 
-try:
- print(new_list[5])
-except Exception as e:
- print(e)
+if 1 in new_list: print(True)
+
+number = []
+print(len(number))
+number.append(2)
+number.append(200)
+print(len(number))
+# try:
+#  print(new_list[5])
+# except Exception as e:
+#  print(e)
 #  traceback.print_exc()
