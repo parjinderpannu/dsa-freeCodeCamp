a893e73f72b761b7ecb309717ed45df927a868bf
parjinder <parjinderpannu@gmail.com>
2024-10-15 13:22:02 -0700

    recursive_binary_search.py

--- recursive_binary_seach.py
+++ recursive_binary_seach.py
@@ -1,5 +1,5 @@
 def recursive_binary_search(list, target):
-    if len(list) == 0
+    if len(list) == 0:
         return False
     else:
         midpoint = (len(list))//2
@@ -8,3 +8,18 @@ def recursive_binary_search(list, target):
             return True
         else:
             if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint+1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint],target)
+            
+
+def verify(result):
+    if result == False:
+        print("Target is not present in list")
+    else:
+        print("Target is present in the list")
+
+l1 = [1,2,3,4,5,6,7,8,9,10]
+t1 = 12
+result = recursive_binary_search(l1,t1)
+verify(result)
